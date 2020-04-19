15ccf69dcd1782cc9f7b1cbd0e24cd9c99cbdf1e
ClayData <claytonortiz96@gmail.com>
2020-04-19 18:36:36 -0500

    added db.js, finished

---
+++ public/db.js
@@ -0,0 +1,54 @@
+let db;
+
+const request = window.indexedDB.open("budget", 1);
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", {autoIncrement: true})
+
+}
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event) {
+    console.log("This didn't work")
+};
+
+function saveRecord(record){
+
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+    pendingStore.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+    let getAll = pendingStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const pendingStore = transaction.objectStore("pending");
+                pendingStore.clear();
+            });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
